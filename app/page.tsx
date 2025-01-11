5a1407b08b39ad32d7278a634616d6ce4cf8e11f
wadihissa1 <148622804+wadihissa1@users.noreply.github.com>
2025-01-11 19:30:40 +0200

    deploy ip

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ export default function Chat() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://172.20.10.3:5000/ask', {
+      const response = await fetch('http://20.49.30.0:80/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/utils/chatStream.ts
+++ src/utils/chatStream.ts
@@ -20,7 +20,7 @@ export const OpenAIStream = async (inputCode) => {
 
   const askQuestion = async (question) => {
     try {
-      const response = await fetch('http://172.20.10.3:5000/ask', {
+      const response = await fetch('http://20.49.30.0:80/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
